76e829eaa995377941cd8ac3d3f2a38cab817514
F.G <ghandourfarouk@gmail.com>
2022-11-29 16:49:52 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :games, only: %i[index show destroy] do
+    resources :registration, only: :create
+  end
+  resources :fields, only: :index do
+    resources :games, only: %i[new create]
+  end
 end
